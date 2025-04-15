94d16a7dfddc5ab1d537b39c734ce6911bd691ed
Nátaly Lara Moraes da Silva <151411522+naatylara@users.noreply.github.com>
2025-04-15 20:58:40 -0300

    Exclusão de abas

--- login.php
+++ login.php
@@ -1,29 +1,29 @@
 <?php
-// Inicia a sessão
-session_start();
+    // Inicia a sessão
+    session_start();
 
-// Verifica se o usuário já está logado
-if (isset($_SESSION['usuario'])) {
-    header('Location: ficha.php'); // Redireciona para a página ficha.php se já estiver logado
-    exit();
-}
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Definir credenciais de login para exemplo (em um caso real, você deve validar no banco de dados)
-    $usuarioValido = 'usuario123';
-    $senhaValida = 'senha123';
-
-    $usuario = $_POST['usuario'];
-    $senha = $_POST['senha'];
-
-    // Verifica se as credenciais são válidas
-    if ($usuario === $usuarioValido && $senha === $senhaValida) {
-        $_SESSION['usuario'] = $usuario; // Inicia a sessão com o nome do usuário
-        header('Location: ficha.php'); // Redireciona para a página ficha.php
+    // Verifica se o usuário já está logado
+    if (isset($_SESSION['usuario'])) {
+        header('Location: ficha.php'); // Redireciona para a página ficha.php se já estiver logado
         exit();
-    } else {
-        $erro = 'Credenciais inválidas!';
     }
-}
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Definir credenciais de login para exemplo (em um caso real, você deve validar no banco de dados)
+        $usuarioValido = 'usuario123';
+        $senhaValida = 'senha123';
+
+        $usuario = $_POST['usuario'];
+        $senha = $_POST['senha'];
+
+        // Verifica se as credenciais são válidas
+        if ($usuario === $usuarioValido && $senha === $senhaValida) {
+            $_SESSION['usuario'] = $usuario; // Inicia a sessão com o nome do usuário
+            header('Location: ficha.php'); // Redireciona para a página ficha.php
+            exit();
+        } else {
+            $erro = 'Credenciais inválidas!';
+        }
+    }
 ?>
 
