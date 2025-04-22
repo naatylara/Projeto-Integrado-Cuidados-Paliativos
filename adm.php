f746af71c2dda54723d2d35890fc82f06dab0746
skibadev <lucas.skiba14@gmail.com>
2025-04-22 20:47:33 -0300

    Criação da classe Admin

---
+++ adm.php
@@ -0,0 +1,31 @@
+<?php
+
+class adm extends Usuario{
+    private $permissoes;
+    private $agenda;
+}
+
+public function __construct($permissoes, $agenda){
+    $this->permissoes = permissoes;
+    $this->agenda = agenda;
+}
+
+public function getPermissoes(){
+    return $this -> permissoes;
+}
+
+public function setPermissoes($permissoes){
+    return $this -> permissoes;
+}
+public function getAgenda(){
+    return $this -> agenda;
+}
+public function setAgenda($agenda){
+    return $this -> agenda;
+}
+public function __toString() {
+    return "{$this->permissoes}, {$this->agenda}";
+}
+
+
+?>
